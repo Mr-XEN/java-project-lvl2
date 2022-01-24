a7c3dedf2a9061ac042e20dde3a6d975f9c4c807
mrxen <mrxenkog@gmail.com>
2022-01-25 00:56:30 +0300

    config Picocli

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -11,20 +11,19 @@ import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.util.concurrent.Callable;
 
-@Command(name = "checksum", mixinStandardHelpOptions = true, version = "checksum 4.0",
-        description = "Prints the checksum (MD5 by default) of a file to STDOUT.")
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
 
 public class App {
 
-    @Parameters(index = "0", description = "The file whose checksum to calculate.")
-    private File file;
-
-    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
-    private String algorithm = "MD5";
+//    @Parameters(index = "0", description = "The file whose checksum to calculate.")
+//    private File file;
+//
+//    @Option(names = {"-a", "--algorithm"}, description = "MD5, SHA-1, SHA-256, ...")
+//    private String algorithm = "MD5";
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
