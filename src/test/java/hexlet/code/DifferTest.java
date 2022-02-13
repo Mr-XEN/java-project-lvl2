12b157a8ddd837564452ed4c89a34d291f49d19e
mrxen <mrxenkog@gmail.com>
2022-02-13 16:39:44 +0300

    added yaml parser

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -1,67 +1,64 @@
-//package hexlet.code;
-//
-//import com.fasterxml.jackson.core.JsonParseException;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.io.File;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//
-//public class DifferTest {
-//
-//    private final Map<String, String> testMap1 = new HashMap<>();
-//    private final Map<String, String> testMap2 = new HashMap<>();
-//
-//
-//    @BeforeAll
-//    public static void beforeAll() {
-//    }
-//
-//    @BeforeEach
-//    public final void beforeEach() {
-//        testMap1.put("host", "hexlet.io");
-//        testMap1.put("timeout", "50");
-//        testMap1.put("proxy", "123.234.53.22");
-//        testMap1.put("follow", "false");
-//
-//        testMap2.put("timeout", "20");
-//        testMap2.put("verbose", "true");
-//        testMap2.put("host", "hexlet.io");
-//
-//
-//
-//
-//    }
-//
+package hexlet.code;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class DifferTest {
+
+    private final Map<String, String> testMap1 = new HashMap<>();
+    private final Map<String, String> testMap2 = new HashMap<>();
+
+
+    @BeforeAll
+    public static void beforeAll() {
+    }
+
+    @BeforeEach
+    public final void beforeEach() {
+        testMap1.put("host", "hexlet.io");
+        testMap1.put("timeout", "50");
+        testMap1.put("proxy", "123.234.53.22");
+        testMap1.put("follow", "false");
+
+        testMap2.put("timeout", "20");
+        testMap2.put("verbose", "true");
+        testMap2.put("host", "hexlet.io");
+
+
+
+
+    }
+
+    @Test
+    public void testAbsolutePath() {
+        String path = "src/test/resources";
+
+        File file = new File(path);
+        String absolutePath = file.getAbsolutePath();
+
+        System.out.println(absolutePath);
+
+        assertTrue(absolutePath.endsWith("src/test/resources"));
+
+    }
+
 //    @Test
-//    public void testAbsolutePath() {
-//        String path = "src/test/resources";
-//
-//        File file = new File(path);
-//        String absolutePath = file.getAbsolutePath();
-//
-//        System.out.println(absolutePath);
-//
-//        assertTrue(absolutePath.endsWith("src/test/resources"));
-//
+//    public void whenArgsIsWrong() {
+//        byte[] wrong = {1, 2};
+//        Exception exception = assertThrows(JsonParseException.class, () -> Parser2.parseJson(wrong));
+//        String expectedMessage = "Illegal character";
+//        String actualMessage = exception.getMessage();
+//        assertTrue(actualMessage.contains(expectedMessage));
 //    }
-//
-////    @Test
-////    public void whenArgsIsWrong() {
-////        byte[] wrong = {1, 2};
-////        Exception exception = assertThrows(JsonParseException.class, () -> Parser2.parseJson(wrong));
-////        String expectedMessage = "Illegal character";
-////        String actualMessage = exception.getMessage();
-////        assertTrue(actualMessage.contains(expectedMessage));
-////    }
-//
+
 //    @Test
 //    public void differTest1() {
 //        String actual = Differ.generate(testMap1, testMap2);
@@ -91,9 +88,9 @@
 //        String expected = "{";
 //        assertTrue(actual.startsWith(expected));
 //    }
-//
-//
-//
-//}
-//
-//
+
+
+
+}
+
+
